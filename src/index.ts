95335c35bd5582f5eaaffa812cd6888c60cd847c
crud0626 <sisofiy6048@gmail.com>
2022-04-22 16:57:16 +0900

    Add drag and drop controller

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Modal from "./modal";
+import Modal from "./modal.js";
 
 const btnsContainer = document.querySelector(".header_button_container");
 const itemsContainer = document.querySelector(".items_container");
@@ -9,6 +9,48 @@ const addItemForm = document.querySelector(".addItemForm");
 const modal = new Modal();
 
 let lastestRole: string = "";
+
+class DragController {
+    private _draggedItem: HTMLDivElement | null = null;
+
+    add = (element: HTMLDivElement): void => {
+        element.addEventListener("dragstart", (event: DragEvent) => {
+            if(event?.dataTransfer && event.currentTarget instanceof HTMLDivElement) {
+                event.dataTransfer.effectAllowed = 'move';
+                this._draggedItem = event.currentTarget;
+            }
+        });
+    
+        element.addEventListener("dragover", (event: DragEvent) => {
+            event.preventDefault();
+            if (event.currentTarget instanceof HTMLDivElement &&
+                event.target instanceof HTMLElement) {
+                event.currentTarget.style.border = "1px solid #f64435";
+            }
+        });
+    
+        element.addEventListener("dragleave", (event: DragEvent) => {
+            event.stopPropagation();
+            if (event.currentTarget instanceof HTMLDivElement) {
+                event.currentTarget.style.border = "1px solid #3f3f3f";
+            }
+        });
+    
+        element.addEventListener("drop", (event: DragEvent) => {
+            event.stopPropagation();
+            if (itemsContainer instanceof HTMLDivElement && 
+                event.currentTarget instanceof HTMLDivElement &&
+                this._draggedItem instanceof HTMLDivElement) 
+            {
+                event.currentTarget.style.border = "1px solid #3f3f3f";
+                itemsContainer.insertBefore(this._draggedItem, event.currentTarget);
+                this._draggedItem = null;
+            }
+        });
+    }
+}
+
+const dragController = new DragController();
 
 function deleteItem(event: MouseEvent) {
     if(event.target instanceof HTMLButtonElement) {
@@ -31,7 +73,9 @@ function createItem(): void {
     
     const item = document.createElement("div");
     item.className = "item";
+    item.draggable = true;
     item.id = itemID;
+    
 
     const deleteBtn = document.createElement("button");
     deleteBtn.className = "closeBtn";
@@ -80,6 +124,7 @@ function createItem(): void {
     }
     item.appendChild(deleteBtn);
     itemsContainer?.appendChild(item);
+    dragController.add(item);
 }
 
 addItemForm?.addEventListener("submit", (event): void => {
